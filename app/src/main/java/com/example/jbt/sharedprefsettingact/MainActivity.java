7bd780bbf2e2d30bacde061120294a688f53c298
eliranmoshe <kaza.moshe@gmail.com>
2017-03-01 12:16:13 +0200

    with cancgelistener

--- app/src/main/java/com/example/jbt/sharedprefsettingact/MainActivity.java
+++ app/src/main/java/com/example/jbt/sharedprefsettingact/MainActivity.java
@@ -2,27 +2,28 @@ package com.example.jbt.sharedprefsettingact;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-SharedPreferences preferences;
+    SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        preferences= PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-        ((Button)findViewById(R.id.SetBtn)).setOnClickListener(new View.OnClickListener() {
+        preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+
+
+        ((Button) findViewById(R.id.SettingBtn)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(MainActivity.this, setting.class);
+                Intent intent = new Intent(MainActivity.this, setting.class);
                 startActivity(intent);
             }
         });
@@ -32,16 +33,18 @@ SharedPreferences preferences;
     @Override
     protected void onResume() {
         super.onResume();
-        preferences= PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-        String user= preferences.getString("set_background", "white");
-        ((RelativeLayout)findViewById(R.id.activity_main)).setBackgroundColor(Color.parseColor(user));
-        boolean isShowTutorial= preferences.getBoolean("with_tutorial",false);
-        if (isShowTutorial) {
-            ((TextView) findViewById(R.id.TV)).setText("welcome");
+        boolean isshowlogin = preferences.getBoolean("showLoginBtn", false);
+        if (isshowlogin) {
+            ((Button) findViewById(R.id.LoginBtn)).setVisibility(View.INVISIBLE);
+        } else if (!isshowlogin) {
+            ((Button) findViewById(R.id.LoginBtn)).setVisibility(View.VISIBLE);
         }
-        if (!isShowTutorial)
-        {
-            ((TextView) findViewById(R.id.TV)).setText("olduser");
-        }
+        String email = preferences.getString("usernameET", "email@email.com");
+        TextView emailET = (TextView) findViewById(R.id.usenameET);
+        emailET.setText(email);
+        String password = preferences.getString("passwordkey", "password");
+        TextView passwordET = (TextView) findViewById(R.id.passET);
+        passwordET.setText(password);
+
     }
 }
--- app/src/main/java/com/example/jbt/sharedprefsettingact/setting.java
+++ app/src/main/java/com/example/jbt/sharedprefsettingact/setting.java
@@ -1,7 +1,13 @@
 package com.example.jbt.sharedprefsettingact;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
 
 /**
  * Created by jbt on 01/03/2017.
@@ -12,5 +18,63 @@ public class setting extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.setting);
+
+        Preference exitBtn=(Preference) findPreference("exitBtn");
+        exitBtn.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                finish();
+                return true;
+            }
+        });
+
+        EditTextPreference email= (EditTextPreference) findPreference("usernameET");
+        email.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                String email= (String) newValue;
+                if (!email.contains("@"))
+                {
+                    Toast.makeText(setting.this, "incorrect email", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+        EditTextPreference password= (EditTextPreference) findPreference("passwordkey");
+        password.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                String password= (String) newValue;
+                if (password.toCharArray().length<8)
+                {
+                    Toast.makeText(setting.this, "incorrect password", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
+
+
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String email = preferences.getString("usernameET", "email@email.com");
+        if (!email.contains("@"))
+        {
+            preferences.edit().putString("usernameET","email@email.com").commit();
+        }
+        String password=preferences.getString("passwordkey","12345678");
+        if (password.toCharArray().length<8)
+        {
+            preferences.edit().putString("passwordkey","12345678").commit();
+        }
+
+
     }
 }
